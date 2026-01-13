dcaffff9a5df4191605e6fa9110770f0cfadc14a
Revital Levy <revit91@gmail.com>
2026-01-13 14:40:39 +0200

    Updated home component

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Register } from "../account/register/register";
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import { Register } from "../account/register/register";
 })
 export class Home {
   protected registerMode = signal(false);
+  protected accountService = inject(AccountService);
 
   showRegister(value: boolean) {
     this.registerMode.set(value);
